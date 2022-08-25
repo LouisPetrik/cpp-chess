8b8aefaa89a9a9f7c4fab21ded188d67980846d6
Louis <33809479+LouisPetrik@users.noreply.github.com>
2022-08-25 15:14:12 +0200

    Initial Commit

---
+++ cpp-chess/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cpp-chess
+//
+//  Created by Louis Petrik on 25.08.22.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  cout << "Hello World" << endl;
+  return 0;
+}
