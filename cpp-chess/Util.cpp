29ece74675aae510745d65546aba6ac81607ed08
Louis <33809479+LouisPetrik@users.noreply.github.com>
2022-08-25 15:15:59 +0200

    initial structure?

---
+++ cpp-chess/Util.cpp
@@ -0,0 +1,8 @@
+//
+//  Util.cpp
+//  cpp-chess
+//
+//  Created by Louis Petrik on 25.08.22.
+//
+
+#include <stdio.h>
---
+++ cpp-chess/Util.h
@@ -0,0 +1,12 @@
+//
+//  Util.h
+//  cpp-chess
+//
+//  Created by Louis Petrik on 25.08.22.
+//
+
+#ifndef Util_h
+#define Util_h
+
+
+#endif /* Util_h */
